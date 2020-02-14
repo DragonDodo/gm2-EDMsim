6195b0adedc5a77b97204dd26acab6810bd60937
DragonDodo <32217699+DragonDodo@users.noreply.github.com>
2020-02-14 15:40:12 +0000

    Added print statements back in for peak debugging

--- gm2sim.py
+++ gm2sim.py
@@ -36,7 +36,8 @@ class muon:
     #method to boost the muon into the detector frame    
         gamma = 29.3 #'magic' gamma value for g-2
         mu_momentum = 3.094e3 #3.094 GeV/c
-        mu_v = -mu_momentum/(gamma*muonM)        
+        mu_v = mu_momentum/(gamma*muonM)     
+
         
         boostedE = gamma*(self.E-mu_v*self.P*np.cos(self.angle))        
         boostedPz = gamma*(self.P*np.cos(self.angle)-mu_v*self.E)
@@ -44,8 +45,7 @@ class muon:
         
         boostedPTot = np.sqrt(boostedPz**2+Px**2)
         
-        boostedAngle = np.arctan(Px/boostedPz)
-        
+        boostedAngle = np.arctan(Px/boostedPz)        
         
         self.E = boostedE
         self.angle = boostedAngle
@@ -130,7 +130,10 @@ def decayMuons(n,t):
         m.gm2Shift(t)
         m.boost()        
         muonlist.append(m)
-        #print ('Muon', i, ', positron energy ', m.E, 'MeV, momentum', m.P, 'and angle', m.angle)
+        print ('Muon', i, ', positron energy ', m.E, 'MeV, momentum', m.P, 'and angle', m.angle)
+        if m.E < 50:
+            print('E before:', m.oE, 'angle before', m.oA, 'momentum before', m.oP)
+
     
     return muonlist
    
@@ -139,7 +142,7 @@ energies = []
 pangles = []
 momenta = []
 
-
+'''
 npoints = 150
 times = list(np.linspace(0., 30e3, npoints))
 #counts = [2815, 2805, 2794, 2611, 2439, 2373, 2212, 2030, 1861, 1631, 1517, 1354, 1197, 1094, 1048, 962, 889, 970, 1029, 1175, 1291, 1425, 1577, 1850, 1953, 2115, 2312, 2406, 2525, 2629, 2774, 2782, 2804, 2738, 2703, 2507, 2430, 2217, 2097, 1928, 1754, 1602, 1375, 1294, 1164, 1040, 963, 975, 936, 1027, 1068, 1186, 1324, 1550, 1698, 1890, 2001, 2242, 2380, 2458, 2665, 2764, 2782, 2823, 2766, 2715, 2554, 2465, 2269, 2102, 1979, 1783, 1586, 1531, 1394, 1233, 1053, 1033, 995, 957, 977, 1071, 1186, 1314, 1440, 1570, 1799, 2028, 2128, 2240, 2469, 2542, 2667, 2794, 2724, 2802, 2670, 2613, 2551, 2372, 2245, 2004, 1834, 1661, 1535, 1341, 1299, 1106, 1045, 1050, 936, 939, 1039, 1048, 1291, 1383, 1532, 1776, 1935, 2037, 2273, 2395, 2545, 2706, 2674, 2812, 2725, 2745, 2635, 2625, 2540, 2326, 2202, 1968, 1813, 1635, 1433, 1327, 1215, 1080, 956, 982, 1013, 995, 1073, 1186, 1261, 1474, 1676, 1816]
@@ -166,19 +169,19 @@ plt.xlabel('Time [ns]')
 plt.ylabel('Number of positrons with E>2000 MeV')
 #plt.legend()
 
-
+'''
 
  
-'''
+
 
 genmuons = decayMuons(n_muons,0.)
 
 #get information out of all muon decays
 for i in genmuons:
-    if i.E < 100:
+    if i.E > 0:
         energies.append(i.E)
         pangles.append(i.angle)
-        momenta.append(i.oP)
+        momenta.append(i.P)
         
 
 
@@ -222,7 +225,7 @@ plt.hist2d(pangles, energies, (50, 50), cmap=plt.cm.jet)
 plt.xlabel('Angle from momentum direction')
 plt.ylabel('Positron energy [MeV]')
 
-'''
+
 
 plt.show()
 
