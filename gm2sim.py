f077191a910daf55eb4b04a77e625bcf7e9cbe0d
DragonDodo <32217699+DragonDodo@users.noreply.github.com>
2020-02-26 16:58:52 +0000

    Fixed cos angle bug, large peak is back.

--- gm2sim.py
+++ gm2sim.py
@@ -26,6 +26,7 @@ class muon:
         self.angle=sample_angle(1,eE)
         self.lifetime = 2.1969811e-6 #seconds
         self.P = np.sqrt(self.E**2-eM**2)
+        self.vangle = vertical_angle()
         
         #unchanged self variables for debugging
         self.oE = self.E
@@ -38,20 +39,53 @@ class muon:
         mu_momentum = 3.094e3 #3.094 GeV/c
         mu_v = -mu_momentum/(gamma*muonM)  
         
-        var = abs(self.angle)%(2*np.pi)
-        if var > np.pi/2:
-            eP = -self.P
+        
+            
+          
+            
+        #if self.angle < 0.:
+            #A = A + 2*np.pi
+        A = self.angle  
+        #var = abs(self.angle)%(2*np.pi)
+        eP = self.P
+        
+        #if var > np.pi:
+            #var = var - 2*np.pi
+            
+            
+            
+        if abs(A) > np.pi/2:
+            eP = -eP
+            A = np.pi - self.angle  
+            
+            #print('backward,', eP*np.cos(A))
+            boostedE = gamma*(self.E-mu_v*eP*np.cos(A))        
+            boostedPz = gamma*(eP*np.cos(A)-mu_v*self.E)
+            Px = eP*np.sin(A)
+        
+            boostedPTot = np.sqrt(boostedPz**2+Px**2)
+        
+            boostedAngle = np.arctan(Px/boostedPz) 
+        elif abs(A) <= np.pi/2:
+            eP = eP
+            A = self.angle  
+            
+            #print('forward,', eP*np.cos(A))
+            boostedE = gamma*(self.E-mu_v*eP*np.cos(A))        
+            boostedPz = gamma*(eP*np.cos(A)-mu_v*self.E)
+            Px = eP*np.sin(A)
+        
+            boostedPTot = np.sqrt(boostedPz**2+Px**2)
+        
+            boostedAngle = np.arctan(Px/boostedPz)
         else:
-            eP = self.P
+            print('o no')
+            
+            
+        #if A < np.pi/2 or A > 3*np.pi/2:
+            #eP=-eP
 
-        
-        boostedE = gamma*(self.E-mu_v*eP*np.cos(self.angle))        
-        boostedPz = gamma*(eP*np.cos(self.angle)-mu_v*self.E)
-        Px = eP*np.sin(self.angle)
-        
-        boostedPTot = np.sqrt(boostedPz**2+Px**2)
-        
-        boostedAngle = np.arctan(Px/boostedPz)        
+               
         
         self.E = boostedE
         self.angle = boostedAngle
@@ -61,9 +95,10 @@ class muon:
     def gm2Shift(self,t):
     #method to add in the g-2 oscillation by rotating spin vector by an angle
     #IMPORTANT: must do this before boosting!
-        w = 1.5e-3
-        shift = w*t
-        self.angle=self.angle+shift
+        #w = 1.5e-3
+        #shift = w*t
+        #self.angle=self.angle+shift
+        self.angle = self.angle+t
         
 
 #-------------------------------------------------------
@@ -124,7 +159,12 @@ def sample_angle(n,E):
 
      
 #--------------------------------------------------------    
-#lifetime dist
+#vertical dists
+        
+def vertical_angle():
+    vA = np.random.normal(0,0.1)
+    return vA    
+    
 
 #--------------------------------------------------------
 
@@ -137,7 +177,7 @@ def decayMuons(n,t):
         m.gm2Shift(t)
         m.boost()        
         muonlist.append(m)
-        #print ('Muon', i, ', positron energy ', m.E, 'MeV, momentum', m.P, 'and angle', m.angle)
+        #print ('Muon', i,', positron energy ', m.E, 'MeV, momentum', m.P, 'and angle', m.angle)
         #if m.E < 50:
             #print('E before:', m.oE, 'angle before', m.oA, 'momentum before', m.oP)
 
@@ -148,8 +188,9 @@ def decayMuons(n,t):
 energies = []
 pangles = []
 momenta = []
-
+vangles = []
 '''
+
 npoints = 100
 times = list(np.linspace(0., 30e3, npoints))
 #counts = [2815, 2805, 2794, 2611, 2439, 2373, 2212, 2030, 1861, 1631, 1517, 1354, 1197, 1094, 1048, 962, 889, 970, 1029, 1175, 1291, 1425, 1577, 1850, 1953, 2115, 2312, 2406, 2525, 2629, 2774, 2782, 2804, 2738, 2703, 2507, 2430, 2217, 2097, 1928, 1754, 1602, 1375, 1294, 1164, 1040, 963, 975, 936, 1027, 1068, 1186, 1324, 1550, 1698, 1890, 2001, 2242, 2380, 2458, 2665, 2764, 2782, 2823, 2766, 2715, 2554, 2465, 2269, 2102, 1979, 1783, 1586, 1531, 1394, 1233, 1053, 1033, 995, 957, 977, 1071, 1186, 1314, 1440, 1570, 1799, 2028, 2128, 2240, 2469, 2542, 2667, 2794, 2724, 2802, 2670, 2613, 2551, 2372, 2245, 2004, 1834, 1661, 1535, 1341, 1299, 1106, 1045, 1050, 936, 939, 1039, 1048, 1291, 1383, 1532, 1776, 1935, 2037, 2273, 2395, 2545, 2706, 2674, 2812, 2725, 2745, 2635, 2625, 2540, 2326, 2202, 1968, 1813, 1635, 1433, 1327, 1215, 1080, 956, 982, 1013, 995, 1073, 1186, 1261, 1474, 1676, 1816]
@@ -181,16 +222,29 @@ plt.ylabel('Number of positrons with E>2000 MeV')
 '''
 olda = []
 
+testangle = np.pi
 
-genmuons = decayMuons(n_muons,0.)
+genmuons = decayMuons(n_muons,testangle)
 
 #get information out of all muon decays
 for i in genmuons:
+    
     if i.E > 0:
+        
+
         energies.append(i.E)
         pangles.append(i.angle)
         momenta.append(i.P)
         olda.append(i.oA)
+        vangles.append(i.vangle)
+        #print('Energy,angle,momentum:',i.oE,i.oA,i.oP,'//',i.E,i.angle,i.P)
+            
+        
+    
+    
+    
+
+        
         
 
 
@@ -207,19 +261,21 @@ distN = [i*1/float(spint.simps(dist,angles)) for i in dist]
 
 #plot positron energy spectrum
 plt.figure(1)
-plt.hist(energies,bins=50,histtype='step',normed=True,label='')
+plt.hist(energies,bins=50,histtype='step',normed=True,label='Forward positrons, aparr')
 #plt.scatter(es,especN,marker='.')
 plt.xlabel('Positron energy [MeV]')
 plt.ylabel('Count [arbitrary units]')
+plt.legend()
 #plt.ylim(0,0.05)
-
+'''
 #plot positron angle spectrum
 plt.figure(2)
-plt.hist(pangles,bins=50,histtype='step')
-plt.hist(olda,bins=50,histtype='step')
+plt.hist(pangles,bins=50,histtype='step',label='Boosted')
+plt.hist(olda,bins=50,histtype='step',label='Unboosted')
 #plt.scatter(angles,distN,marker='.')
 plt.xlabel('Angle from momentum direction')
 plt.ylabel('Count [arbitrary units]')
+plt.legend()
 #plt.ylim(0,0.25)
 
 plt.figure(3)
@@ -234,6 +290,9 @@ plt.hist2d(np.cos(pangles), energies, (50, 50), cmap=plt.cm.jet)
 plt.xlabel('Angle from momentum direction')
 plt.ylabel('Positron energy [MeV]')
 
+plt.figure(5)
+plt.hist(vangles,bins=50,histtype='step')
+'''
 
 
 plt.show()
