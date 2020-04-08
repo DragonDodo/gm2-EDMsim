05e063cf5c080126dbba6c183c5535d8930e807c
DragonDodo <32217699+DragonDodo@users.noreply.github.com>
2020-04-08 16:31:32 -0500

    New EDM shift method

--- gm2sim.py
+++ gm2sim.py
@@ -11,8 +11,9 @@ TODO:
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as spint
-from scipy.stats import norm
+from scipy.stats import norm, cauchy
 from scipy.optimize import curve_fit
+
 
 
 
@@ -24,7 +25,7 @@ from scipy.optimize import curve_fit
 
 
 option = "EDM"
-npoints = 100 #number of points to plot for wiggles
+npoints = 1000 #number of points to plot for wiggles
 
 
 #MeV units
@@ -56,9 +57,15 @@ class muon:
         gamma = 29.3 #'magic' gamma value for g-2
         mu_momentum = 3.094e3 #3.094 GeV/c
         mu_v = -mu_momentum/(gamma*muonM) #-tive because boosting back into lab frame
+        '''
         
+        if abs(self.oV) > np.pi/2:
+            self.angle = (self.angle+np.pi)%(np.pi*2)
+            
+            self.vangle = -np.pi+self.vangle
+           
                 
-        
+        '''
         Px = self.P*np.sin(self.angle)*np.cos(self.vangle)
         Py = self.P*np.sin(self.vangle)
         Pz = self.P*np.cos(self.angle)*np.cos(self.vangle)
@@ -77,6 +84,7 @@ class muon:
     #IMPORTANT: must do this before boosting!
         w = 1.5e-3
         shift = w*t
+        #shift = t
         self.angle=self.angle+shift
         self.oA = self.angle 
         
@@ -87,13 +95,24 @@ class muon:
         wEDM = 1.5e-3 # ~ g-2 freq   
         #add in a direct calculation here!
         
-        d_u = 10*1.9e-19 #100BNL
+        d_u = 10*1.9e-19 #10BNL
         consts = 8.9e15
         
         A = d_u*consts
-        phi = np.pi/2# EDM is pi/2 out of phase with g-2 oscillation        
-        self.vangle = self.vangle + A*np.cos(wEDM*t-phi)   
-        #self.vangle = self.vangle + t
+        phase = np.pi/2# EDM is pi/2 out of phase with g-2 oscillation        
+        dphi = A*np.cos(wEDM*t-phase) 
+        
+        #dphi = t
+        theta  = self.angle
+        phi  = self.vangle
+        
+        tangle = np.sin(theta)/(np.cos(theta)*np.cos(dphi)-np.tan(phi)*np.sin(dphi))
+        
+        svangle = np.cos(theta)*np.cos(phi)*np.sin(dphi)+np.sin(phi)*np.cos(dphi)
+        
+        self.angle = np.arctan(tangle)
+        self.vangle = np.arcsin(svangle)
+        
 
 #-------------------------------------------------------
 #Define the energy distribution of muon decay and sample
@@ -195,9 +214,10 @@ def decayMuons(n,t,e):
     
     for i in range(n+1):
         m = muon()
-        m.gm2Shift(t) #apply g-2 precession
         m.EDMShift(e) #apply EDM precession
+        m.gm2Shift(t) #apply g-2 precession        
         m.boost() #boost into lab frame  
+        
         #m.boostback()
         muonlist.append(m)  
     return muonlist
@@ -241,7 +261,7 @@ if option == "GM2":
 
 elif option == "EDM":
     
-    times = list(np.linspace(0., 30e3, npoints))
+    times = list(np.linspace(0., 100e3, npoints))
     
     avAngle = []
     spreadAngle = []
@@ -253,7 +273,7 @@ elif option == "EDM":
         edmlist = []
         genmuons = decayMuons(n_muons,t,t)
         
-        #gm2
+        
         for i in genmuons:
             edmlist.append(i.vangle)
             
@@ -272,22 +292,22 @@ elif option == "EDM":
         pars, cov = curve_fit(lambda x, mu, sig : norm.pdf(x, loc=mu, scale=sig), centers, n, p0=[1,1])
         
         
-        
-        #p = n/100
-        #yerr = np.sqrt(np.sqrt(100)*p*(1-p)*1.95)
-        #plt.errorbar(centers, n, yerr, fmt='C0_')
-        
-        
-        #xs = np.linspace(-0.5,0.5,200)
+        '''
+        p = n/100
+        yerr = np.sqrt(np.sqrt(100)*p*(1-p)*1.95)
+        plt.errorbar(centers, n, yerr, fmt='C0_')
         
         
+        xs = np.linspace(-0.5,0.5,200)
         
-        #fit = [norm.pdf(i,pars[0],pars[1]) for i in xs]
-        #plt.plot(xs,fit, linewidth = 2,label = r'$\mu = 1.45x10^{-5}, \sigma = 0.010$')
-        #plt.xlabel('Vertical angle [rad]')
-        #plt.ylabel('Count [arbitrary units]')
+        
+        
+        fit = [cauchy.pdf(i,pars[0],pars[1]) for i in xs]
+        plt.plot(xs,fit, linewidth = 2,color='orange')
+        plt.xlabel('Vertical angle [rad]')
+        plt.ylabel('Count [arbitrary units]')
         #plt.legend()
-        
+        '''
                
         m = pars[0]
         e = np.sqrt(cov[0,0])
@@ -295,7 +315,7 @@ elif option == "EDM":
         
         
         #m = np.mean(edmlist)
-        #s = np.std(edmlist)/np.sqrt(n_muons)
+        s = np.std(edmlist)/np.sqrt(n_muons)
 
         
         avAngle.append(m)
@@ -341,154 +361,195 @@ else:
     othervan = []
     ov = []
     
-    GM2_fixedangle = 0
-    EDM_fixedangle = 1
+    GM2 = 0
+    EDM = 0
     
-    genmuons = decayMuons(n_muons,GM2_fixedangle,EDM_fixedangle)
-            
+    edmlist = [0,np.pi/2,np.pi]    
     
-    #get information out of all muon decays
-    for i in genmuons:
-        
-        vangles.append(i.vangle)
-        ov.append(i.oV)
-        
-        
-        
-        if i.E > 0:     
+    for gm2 in edmlist:
     
-            energies.append(i.E)
-            pangles.append(i.angle)
-            momenta.append(i.P)
-            olda.append(i.oA)
-            vangles.append(i.vangle)
-            ov.append(i.oV)
-            print ('Muon', genmuons.index(i), ',positron energy ', i.E, 'MeV, momentum', i.P, 'and angle', i.angle)  
-            
-
-        
-        
-    
+        genmuons = decayMuons(n_muons,gm2,0.01)
                 
+        
+        #get information out of all muon decays
+        for i in genmuons:
             
-    #These plots can be used to check the samples follow the required distributions
-    #Not needed unless you've tampered with those
-    '''   
-    #define checking plots for distributions
-    es = range(0,54,1)
-    espec = [energyDecaySpectrum(i) for i in es]
-    especN = [i*1/float(spint.simps(espec,dx=1)) for i in espec]
+            ov.append(i.oV)
+            vangles.append(i.vangle)
+            #ov.append(i.oV)
+            
+            #print('V_angle before:', i.oV, 'V_angle after', i.vangle)
+            '''
+            if i.oV > np.pi/2-edm:
+                vangles.append(i.vangle)
+                f=open('bumpevents.txt','a')   
+                #oldE, oldp, oldA, oldV, newV
+                f.write(str(i.E)+','+str(i.P)+ ','+str(i.angle)+','+ str(i.oV) + ',' + str(i.vangle)+'\n')
+                f.close()
+                print(genmuons.index(i))
+                
+            elif i.oV <= np.pi/2-edm:
+                othervan.append(i.vangle)
+            
+            '''
+            '''
+            if i.E > 0:     
+        
+                energies.append(i.E)
+                pangles.append(i.angle)
+                momenta.append(i.P)
+                olda.append(i.oA)
+                vangles.append(i.vangle)
+                ov.append(i.oV)
+                print ('Muon', genmuons.index(i), ',positron energy ', i.E, 'MeV, momentum', i.P, 'and angle', i.angle)  
+                
+            '''
+            
+            
+        
+                    
+                
+        #These plots can be used to check the samples follow the required distributions
+        #Not needed unless you've tampered with those
+        '''   
+        #define checking plots for distributions
+        es = range(0,54,1)
+        espec = [energyDecaySpectrum(i) for i in es]
+        especN = [i*1/float(spint.simps(espec,dx=1)) for i in espec]
+        
+        angles = np.arange(-np.pi,np.pi,0.1)
+        dist = [1 + (1/3)*np.cos(theta) for theta in angles]
+        distN = [i*1/float(spint.simps(dist,angles)) for i in dist]
+        '''
+        
+        '''
+        #plot positron energy spectrum
+        plt.figure(1)
+        plt.hist(energies,bins=50,histtype='step',normed=True)
+        #plt.scatter(es,especN,marker='.')
+        plt.xlabel('Positron energy [MeV]')
+        plt.ylabel('Count [arbitrary units]')
+        #plt.ylim(0,0.05)
+        
+        #plot positron angle spectrum
+        plt.figure(2)
+        #plt.hist(pangles,bins=50,histtype='step',label='Boosted')
+        plt.hist((olda),bins=50,histtype='step',label='Unboosted')
+        #plt.scatter(angles,distN,marker='.')
+        plt.xlabel('cos(angle), pre boost')
+        plt.ylabel('Count [arbitrary units]')
+        #plt.legend()
+        #plt.ylim(0,0.25)
+        
+        #plot momentum spectrum
+        plt.figure(3)
+        plt.hist(momenta,bins=50,histtype='step',normed=True)
+        plt.xlabel('Positron momentum [MeV/c]')
+        plt.ylabel('Count [arbitrary units]')
+        
+        
+        #plot 2d angle vs energy 
+        plt.figure(4)
+        plt.hist2d(pangles, energies, (50, 50), cmap=plt.cm.jet)
+        plt.xlabel('Angle from momentum direction')
+        plt.ylabel('Positron energy [MeV]')
+        plt.colorbar()
+        #plt.xlim(0.925,1.)
+        '''
     
-    angles = np.arange(-np.pi,np.pi,0.1)
-    dist = [1 + (1/3)*np.cos(theta) for theta in angles]
-    distN = [i*1/float(spint.simps(dist,angles)) for i in dist]
-    '''
-    
-    '''
-    #plot positron energy spectrum
-    plt.figure(1)
-    plt.hist(energies,bins=50,histtype='step',normed=True)
-    #plt.scatter(es,especN,marker='.')
-    plt.xlabel('Positron energy [MeV]')
-    plt.ylabel('Count [arbitrary units]')
-    #plt.ylim(0,0.05)
-    
-    #plot positron angle spectrum
-    plt.figure(2)
-    #plt.hist(pangles,bins=50,histtype='step',label='Boosted')
-    plt.hist((olda),bins=50,histtype='step',label='Unboosted')
-    #plt.scatter(angles,distN,marker='.')
-    plt.xlabel('cos(angle), pre boost')
-    plt.ylabel('Count [arbitrary units]')
-    #plt.legend()
-    #plt.ylim(0,0.25)
-    
-    #plot momentum spectrum
-    plt.figure(3)
-    plt.hist(momenta,bins=50,histtype='step',normed=True)
-    plt.xlabel('Positron momentum [MeV/c]')
-    plt.ylabel('Count [arbitrary units]')
-    
-    
-    #plot 2d angle vs energy 
-    plt.figure(4)
-    plt.hist2d(pangles, energies, (50, 50), cmap=plt.cm.jet)
-    plt.xlabel('Angle from momentum direction')
-    plt.ylabel('Positron energy [MeV]')
-    plt.colorbar()
-    #plt.xlim(0.925,1.)
-    '''
+        #plot vertical angle distribution
+        plt.figure(5)
+        minx = -0.2
+        maxx = 0.2
+        bins = np.linspace(minx,maxx,100)
+        #bins = 50
+        
+        #plt.hist(othervan,bins=bins,histtype='step',label=r'Inside')
+        #plt.hist(vangles,bins=bins,histtype='step',label=r'Outside')
+        
+        
+        
+        
+        
+        #plt.xlim(-np.pi/2,np.pi/2)
+        
+        n, bins = np.histogram(vangles,bins,density=True)
+        
+        centers = (0.5*(bins[1:]+bins[:-1]))
+        pars, cov = curve_fit(lambda x, mu, sig : norm.pdf(x, loc=mu, scale=sig), centers, n, p0=[1,1])
+        
+        print(pars[0],pars[1])
+        
+        err = np.sqrt(cov[0,0])
+        
+        x = np.linspace(minx,maxx,200)
+        
+        plt.hist(vangles,bins=bins,histtype='step',density=True,label=str(gm2))
+        
 
-    #plot vertical angle distribution
-    plt.figure(5)
-    bins = np.linspace(-0.5,0.5,200)
+        plt.legend()
+        
+        #plt.legend()
+        
+        
+        #plt.plot(x,norm.pdf(x,pars[0],pars[1]))
     
-    plt.hist(vangles,bins=bins,histtype='step',normed=True)
+        
+        
+        '''
+        plt.hist(othervan, bins=600,histtype='step',label=r'$\theta_{V} \leq \pi/4$')
+        plt.hist(vangles, bins=50,histtype='step',label=r'$\theta_{V} > \pi/4$')
+        
+        #plt.title(r'$lalala > \beta$')
+        
+        tsum = vangles+othervan
+        plt.hist(tsum,bins=600, histtype='step',label='Sum')
+        '''
+        '''
+        xt = plt.xticks()[0]  
+        xmin, xmax = min(xt), max(xt)  
+        lnspc = np.linspace(xmin, xmax, len(vangles))
     
-    #plt.xlim(-np.pi/2,np.pi/2)
-    
-    n, bins = np.histogram(vangles,bins,normed=True)
-    
-    centers = (0.5*(bins[1:]+bins[:-1]))
-    pars, cov = curve_fit(lambda x, mu, sig : norm.pdf(x, loc=mu, scale=sig), centers, n, p0=[1,1])
-    
-    print(pars[0],pars[1])
-    
-    x = np.linspace(-0.5,0.5,200)
-    
-    plt.plot(x,norm.pdf(x,pars[0],pars[1]))
-
-    
-    
-    '''
-    plt.hist(othervan, bins=600,histtype='step',label=r'$\theta_{V} \leq \pi/4$')
-    plt.hist(vangles, bins=50,histtype='step',label=r'$\theta_{V} > \pi/4$')
-    
-    #plt.title(r'$lalala > \beta$')
-    
-    tsum = vangles+othervan
-    plt.hist(tsum,bins=600, histtype='step',label='Sum')
-    '''
-    '''
-    xt = plt.xticks()[0]  
-    xmin, xmax = min(xt), max(xt)  
-    lnspc = np.linspace(xmin, xmax, len(vangles))
-
-    
-    m, s = stats.norm.fit(vangles) 
-    pdf_g = stats.norm.pdf(lnspc, m, s)   
-    plt.plot(lnspc, pdf_g, label="Norm") 
-    
-    #plt.plot(lnspc,stats.norm.pdf(lnspc,np.mean(vangles), np.std(vangles)))
-    
-    #plt.hist(vangles,bins=50,histtype='step')
-    '''
-    plt.xlim(-0.5,0.5)
-    plt.xlabel('Vertical angle [rad]')
-    plt.ylabel('Count [arbitrary units]')
-    #plt.legend()
-    '''
-    xs = np.linspace(-1.5,1.5,1000)
-    g1 = stats.norm.pdf(xs,-0.00012,0.03221)*0.21
-    g2 = stats.norm.pdf(xs,0.00165,0.72951)
-    plt.plot(xs,g1,label=r'$\mu = -0.00012, \sigma =0.03221$')
-    plt.plot(xs,g2,label=r'$\mu = 0.00165, \sigma =0.72951$')
-    plt.legend()
-    '''
-    #angles = np.linspace(-np.pi/2,np.pi/2,100)
-    #checks = [0.5*verticalAngleSpectrum(theta) for theta in angles]
-    #checksN = [i*1/float(spint.simps(checks,dx=1)) for i in checks]
-   # u = np.random.normal(0,0.8,n_muons)
-    #plt.hist(u, normed = True, histtype='step')
-    
-    
-    #plt.plot(angles,checks)
-    
-    
-   
-    #plt.plot(x,z)
-    
-    
-    #plt.show()
+        
+        m, s = stats.norm.fit(vangles) 
+        pdf_g = stats.norm.pdf(lnspc, m, s)   
+        plt.plot(lnspc, pdf_g, label="Norm") 
+        
+        #plt.plot(lnspc,stats.norm.pdf(lnspc,np.mean(vangles), np.std(vangles)))
+        
+        #plt.hist(vangles,bins=50,histtype='step')
+        '''
+        #plt.xlim(-0.5,0.5)
+        plt.xlabel('Vertical angle [rad]')
+        plt.ylabel('Count [arbitrary units]')
+        #plt.legend()
+        '''
+        xs = np.linspace(-1.5,1.5,1000)
+        g1 = stats.norm.pdf(xs,-0.00012,0.03221)*0.21
+        g2 = stats.norm.pdf(xs,0.00165,0.72951)
+        plt.plot(xs,g1,label=r'$\mu = -0.00012, \sigma =0.03221$')
+        plt.plot(xs,g2,label=r'$\mu = 0.00165, \sigma =0.72951$')
+        plt.legend()
+        '''
+        #angles = np.linspace(-np.pi/2,np.pi/2,100)
+        #checks = [0.5*verticalAngleSpectrum(theta) for theta in angles]
+        #checksN = [i*1/float(spint.simps(checks,dx=1)) for i in checks]
+       # u = np.random.normal(0,0.8,n_muons)
+        #plt.hist(u, normed = True, histtype='step')
+        
+        
+        #plt.plot(angles,checks)
+        
+        
+       
+        #plt.plot(x,z)
+        
+        
+        #plt.show()
+        '''
+        f=open('scan.txt','a')    
+        f.write(str(edm)+','+str(pars[0])+ ','+str(err)+'\n')
+        f.close()
+        '''
 
 
