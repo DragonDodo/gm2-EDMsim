e554d7c8a95dea6cf8aa6b695ba219dabedf043a
Dominika Vasilkova <dominika.vasilkova@gmail.com>
2020-05-23 14:43:18 +0100

    Made CLs method importable

--- CLs.py
+++ CLs.py
@@ -4,13 +4,7 @@
 #script to implement the CLs method for setting limits
 '''
 
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import norm
-
-#make plots in g-2 style
-plt.style.use('gm2.mplstyle')
-plt.ion()
 
 def pVal(mean,sigma,x):
     #functional shorthand to calc p-value
@@ -24,7 +18,7 @@ def findCLsLimit(xrange,amp,err,sig):
         pb = pVal(0,err,t)
         psb = pVal(amp,err,t)
         CLs = psb/pb
-        #print(t, CLs)
+        print(t, CLs, pb, psb)
         
         if CLs <= sig: #iterate over bins to find limit CLs = sig
             limit = t
@@ -32,30 +26,40 @@ def findCLsLimit(xrange,amp,err,sig):
     
     con_limit = limit + (xrange[1]-xrange[0]) #add extra bin to overcover
     return con_limit
-            
-xrange = np.linspace(0,100e-19,1000) #this range is what you scan over: so setting it from 0 limits the edm > 0. 
-amp = 0.3e-19
-err = 2e-19
+ 
+if __name__ == "__main__":  
+    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    #make plots in g-2 style
+    plt.style.use('gm2.mplstyle')
+    plt.ion()  
 
-amps = np.linspace(0e-19,10e-19,40) #input du values (not really amplitudes any more)
-
-clist = []
-olist = []
-
-for i in amps:
-    sol = findCLsLimit(xrange,i,err,0.95) # CLs   
-    pb = norm.interval(0.95,loc=i,scale=err)[1] #old but two-tailed method
-    clist.append(sol)
-    olist.append(pb)
+           
+    xrange = np.linspace(0,100e-19,1000) #this range is what you scan over: so setting it from 0 limits the edm > 0. 
+    amp = 0.3e-19
+    err = 2e-19
     
+    amps = np.linspace(0e-19,10e-19,40) #input du values (not really amplitudes any more)
     
-plt.figure(1)  
-plt.scatter(amps,clist,label='CLs method')  
-plt.scatter(amps,olist,label='Naive frequentist')
-plt.xlim(min(amps),max(amps))
-plt.ylim(min(clist),max(olist))
-plt.xlabel(r"$d_{u}\ from\ amplitude\ [e\ cm]$",horizontalalignment='right', x=1.0, verticalalignment='bottom', y=0.0, labelpad=40)
-plt.ylabel("Limit set [e cm]",horizontalalignment='right', y=1.0, verticalalignment='bottom', x=0.0,labelpad=20)
+    clist = []
+    olist = []
     
-    
+    for i in amps:
+        sol = findCLsLimit(xrange,i,err,0.95) # CLs   
+        pb = norm.interval(0.95,loc=i,scale=err)[1] #old but two-tailed method
+        clist.append(sol)
+        olist.append(pb)
+        
+        
+    plt.figure(1)  
+    plt.scatter(amps,clist,label='CLs method')  
+    plt.scatter(amps,olist,label='Naive frequentist')
+    plt.xlim(min(amps),max(amps))
+    plt.ylim(min(clist),max(olist))
+    plt.xlabel(r"$d_{u}\ from\ amplitude\ [e\ cm]$",horizontalalignment='right', x=1.0, verticalalignment='bottom', y=0.0, labelpad=40)
+    plt.ylabel("Limit set [e cm]",horizontalalignment='right', y=1.0, verticalalignment='bottom', x=0.0,labelpad=20)
+        
+        
     
