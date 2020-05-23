0ebd656761c23813b84acbfe5514f80b563f758f
Dominika Vasilkova <dominika.vasilkova@gmail.com>
2020-05-23 14:23:36 +0100

    Fixed bugs

--- FC.py
+++ FC.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue May 19 11:03:02 2020
 
-@author: dodo
-"""
 #FC implementation
 
 import numpy as np
@@ -14,21 +10,14 @@ from scipy.stats import norm
 plt.style.use('gm2.mplstyle')
 plt.ion()
 
-
-#start with a test mu value to find the limit for 
-#scan across x, find the R ratio of that gaussian/the mu best gaussian
-#order them based on R and add x points to the band until the prob in the band hits desired sig
-#get min/max from that for each mu: that defines the band 
-
-#this code only tries to calculate one mu atm 
-
-def FC(x_bins,width,sig,val):
+def FC(x_bins,mu_bins,width,sig):
     #x_bins - range of x to scan over
     #width - standard deviation of the gaussian
     #sig - desired significance level 
     #val - measured x to set the limit using
+    print('Calculating band...')
     
-    def Rscan(bins):
+    def Rscan(bins,val):
         xwidth = (max(bins)-min(bins))/len(bins)
         Rs = []          
     
@@ -39,72 +28,124 @@ def FC(x_bins,width,sig,val):
                 mu_max = xs
             elif xs < 0:
                 mu_max = 0
-
-                
-            '''
-            #this is the same calculation done analytically, also doesn't work
-            if xs >= 0:
-                R = np.exp(-0.5*(xs-val)*(xs-val))
-            elif xs < 0:
-                R = np.exp(0.5*(xs*val-val*val))
-            '''
-                       
+    
             #R ratio P(x|mu)/P(x|mu_max)
-            R = norm(val,width).pdf(xs)/norm(mu_max,width).pdf(xs)            
+            R = norm(val,1).pdf(xs)/norm(mu_max,1).pdf(xs)            
             Rs.append([xs,R])      
     
         #sort by R value, largest first    
         Rarr = sorted(Rs, key=lambda x: x[1],reverse=True)
-        #print(Rarr)
         
         psum = 0 
         xlimvals = []
 
-
         #numerically integrate out adding intervals of highest R first
         for p in Rarr:                       
             
-            pv = norm.pdf(p[0],val,width)        
-            #ab0 = norm.cdf(0,val,width) 
+            pv = norm.pdf(p[0],val,1)        
             prob = pv*xwidth #have to scale the pdf prob as it's not normalised right
             psum += prob
             xlimvals.append(p[0])
-            #print(psum)
+
                
             if psum > sig:
                 break
         
-        #print(xlimvals)
-        #print(psum)
         up = max(xlimvals)   
-        down = min(xlimvals)
-        
+        down = min(xlimvals)        
         
         if up == max(x_bins):
-            print('Warning: upper limit setting not complete, check mu range!')
-         
-                  
-        return down,up+xwidth,xwidth
+            print('Warning: upper limit setting not complete, check mu range!')         
+        #print(down,up)         
+        return down,up
     
-    firstscan = Rscan(x_bins) 
+    band = []
+    for mu in mu_bins:    
+        scan = Rscan(x_bins,mu) 
+        band.append(scan)
+
+    return band
+
+
+def find_FC_limits(band,x_bins,mu_bins,testval): 
+    print('Setting limits...')
     
+    def limscan(xs,mu_bins,testx):
+        for i in range(len(xs)):
+            lim = mu_bins[i]
+            if xs[i] >= testx:
+                break
+            
+        return lim      
+    
+    upx = []
+    downx = []
+    
+    for i,j in zip(band,mu_bins):
+        downx.append(i[1])
+        upx.append(i[0])
+        
+    testx = testval
+        
+    
+    downlim = limscan(downx,mu_bins,testx)
+    uplim = limscan(upx,mu_bins,testx) 
+
+        
+    if downlim < 0:
+        downlim = 0.0
+        
+    return downlim,uplim
+
+
+def FC_limit(x,sigma,x_bins=np.linspace(-10,10,400),mu_bins=np.linspace(0,8,100),alpha=0.9):
+    testx = x/sigma
+    
+    if testx > max(mu_bins):
+        raise ValueError('x/sigma not in band, scan for wider mean value!')
+        
+    rough = FC(x_bins,mu_bins,sigma,alpha)
+    lims = find_FC_limits(rough,x_bins,mu_bins,testx) 
+    
+    print('Limits per sigma:',lims[0],lims[1])
+    print('Scaled limits:',lims[0]*sigma,lims[1]*sigma)
+    
+    return lims[0]*sigma,lims[1]*sigma
+        
+x = 0
+sigma = 1
+
+l = FC_limit(x,sigma)
+print(l)
+
+
    
-    print(firstscan)
 
-    return firstscan
+  
+
+#print(lims[0],lims[1])
+'''
+fct = [-2,-1,0,0.5,1,1.3,1.5,1.7,1.9,2,3] #test x values
+FCpaper = [0.4,0.81,1.64, 2.14, 2.64, 2.94, 3.14,3.34,3.54,3.64, 4.64] #upper limits
+downs = [0,0,0,0,0,0.02,0.22,0.38,0.51,0.58,1.37] #lower limits
+
+  
+plt.figure(1)
+plt.plot(lims[2],mu_bins,color='C0',label = 'This implementation')
+plt.plot(lims[3],mu_bins,color='C0')
+#plt.axvline(testx,color='#d3d3d3',linestyle='--')
+#plt.axhline(lims[0],color='#d3d3d3',linestyle='--')
+#plt.axhline(lims[1],color='#d3d3d3',linestyle='--')
 
 
-
-
-width = 1
-
-
-x_bins=np.linspace(-10,10,100)
-rough = FC(x_bins,width,0.90,0)
-
+plt.xlim(0,7)
+plt.ylim(0,5)
+plt.legend()
+plt.xlabel(r"$x/\sigma$",horizontalalignment='right', x=1.0, verticalalignment='bottom', y=0.0)
+plt.ylabel("$\mu/\sigma$",horizontalalignment='right', y=1.0, verticalalignment='bottom', x=0.0,labelpad=20)
 
 #the values it should be getting are in FCpaper and downs for the up/down limits
-'''
+
 fct = [-2,-1,0,0.5,1,1.3,1.5,1.7,1.9,2,3] #test x values
 FCpaper = [0.4,0.81,1.64, 2.14, 2.64, 2.94, 3.14,3.34,3.54,3.64, 4.64] #upper limits
 downs = [0,0,0,0,0,0.02,0.22,0.38,0.51,0.58,1.37] #lower limits
@@ -128,6 +169,5 @@ plt.plot(fct,downs,color='C1')
 
 #plt.ylim(bottom=0)
 #plt.xlim(0,4)
-plt.xlabel(r"$x/\sigma$",horizontalalignment='right', x=1.0, verticalalignment='bottom', y=0.0)
-plt.ylabel("$\mu/\sigma$",horizontalalignment='right', y=1.0, verticalalignment='bottom', x=0.0,labelpad=20)
+
 '''
